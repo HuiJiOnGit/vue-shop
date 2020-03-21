3230008c39831720808dfe239d9b797d81e03fd5
huiji <Huiji2018@gmail.com>
2020-03-21 17:27:04 +0800

    修改api来源

--- src/main.js
+++ src/main.js
@@ -12,7 +12,8 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 
 Vue.prototype.$http = axios
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://119.23.53.78:8888/api/private/v1'
+//'http://127.0.0.1:8888/api/private/v1/'
 
 // axios 请求拦截
 axios.interceptors.request.use(config => {
