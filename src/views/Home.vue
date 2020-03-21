ab1a79285caf31ac6520a69d7252efcfc7526629
LHR-lhr <lhr180107@gmail.com>
2020-03-21 14:02:09 +0800

    完善样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,9 @@
   <el-container class="home-container">
     <!-- 头部 -->
     <el-header>
-      <div>
+      <div class="logo">
         <img src="../assets/heima.png"
-             alt="">
+             alt="电商管理后台">
         <span>电商管理后台</span>
       </div>
       <el-button @click="logout">登出</el-button>
@@ -14,7 +14,7 @@
       <!-- 侧边栏 -->
       <el-aside :width="isCollapse ? '64px' : '200px'">
         <div class="toggle-button"
-             @click="toggleCollapse">|||</div>
+             @click="toggleCollapse" :class="togglestyle">|||</div>
         <!-- 菜单组件 -->
         <el-menu :default-active="this.$route.path"
                  background-color="rgba(0,0,0,0)"
@@ -44,7 +44,7 @@
         </el-menu>
       </el-aside>
       <!-- mian -->
-      <el-main>
+      <el-main class="welcome">
         <router-view/>
       </el-main>
     </el-container>
@@ -52,89 +52,172 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue"
+  // @ is an alias to /src
+  import HelloWorld from "@/components/HelloWorld.vue"
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      // 左侧菜单
-      menulist: [],
-      isCollapse: false
-    }
-  },
-  methods: {
-    logout() {
-      //清空token
-      window.sessionStorage.clear()
-      //跳转登陆页
-      this.$router.push("/login")
+  export default {
+    name: "Home",
+    components: {
+      HelloWorld
     },
-    async getMenuList() {
-      const { data: res } = await this.$http.get("menus")
-      if (res.meta.status !== 200) {
-        return this.$message.error(res.meta.msg)
+    data() {
+      return {
+        // 左侧菜单
+        menulist: [],
+        isCollapse: false,
+        togglestyle:{toggle:false}
       }
-      this.menulist = res.data
     },
-    toggleCollapse() {
-      this.isCollapse = !this.isCollapse
+    methods: {
+      logout() {
+        //清空token
+        window.sessionStorage.clear()
+        //跳转登陆页
+        this.$router.push("/login")
+      },
+      async getMenuList() {
+        const { data: res } = await this.$http.get("menus")
+        if (res.meta.status !== 200) {
+          return this.$message.error(res.meta.msg)
+        }
+        this.menulist = res.data
+      },
+      toggleCollapse() {
+        this.isCollapse = !this.isCollapse;
+        this.togglestyle.toggle=this.isCollapse
+      }
+    },
+    created() {
+      this.getMenuList()
     }
-  },
-  created() {
-    this.getMenuList()
   }
-}
 </script>
 
 <style lang="less" scoped>
-.home-container {
-  height: 100vh;
-}
-.el-header {
-  //background-color: #BEBEBE;
-  background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
-  color: #000;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding-left: 0;
-  color: black;
-  font-size: 20px;
+  .home-container {
+    height: 100vh;
+  }
+  .el-header {
+    background-color:#409EFF;
+    color: #000;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 0;
+    color: black;
+    font-size: 20px;
   > div {
     display: flex;
     align-items: center;
-    span {
-      margin-left: 15px;
-    }
+  span {
+    margin-left: 15px;
   }
-}
-.el-aside {
-  //background-color: #D3D3D3;
-  background-image: linear-gradient(169deg, #4facfe 0%, #00f2fe 100%);
-  color: #333;
+  }
+  }
+  .el-aside {
+    overflow:hidden;
+    background-color: #fff;
+    border-right:1px solid rgba(128,128,128,.1);
+    color: #333;
   .el-menu {
     border-right: none;
   }
-}
+  }
 
-.toggle-button {
-  font-size: 10px;
-  line-height: 24px;
-  color: black;
-  text-align: center;
-  letter-spacing: 0.3em;
-  cursor: pointer;
-  user-select:none;
-}
+  .toggle-button {
+    line-height: 32px;
+    color: #409EFF;
+    text-align: center;
+    letter-spacing: 0.3em;
+    cursor: pointer;
+    user-select:none;
+    font-size:16px;
+    font-weight:600;
+    transition:all 500ms ease-in-out;
+    margin-top:5px;
+  }
 
-.el-main {
+  .el-main {
   //background-color: #FFFFFF;
-  background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
-  color: #333;
-}
+    /* background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%); */
+    color: #333;
+  }
+  .welcome{
+    backgound:rgba(128,128,128,.1);
+  }
+  /deep/.welcome>div{
+    width:100%;
+  }
+  .logo{
+    width:200px;
+    height:40px;
+    display:inline-block;
+    padding: 0;
+    padding-left:20px;
+    box-sizing:border-box;
+    display:flex;
+    flex-wrap:wrap;
+    align-items:center;
+    justify-content:space-around;
+  >img{
+    height:100%;
+    width:auto;
+    -webkit-filter: brightness(0%) invert(100%);
+    -moz-filter: brightness(0%) invert(100%);
+    -ms-filter: brightness(0%) invert(100%);
+    filter: brightness(0%) invert(100%);
+  }
+  >span{
+    font-size:18px;
+    font-weight:600;
+    /* color:#409EFF; */
+    color:#fff;
+  }
+  }
+  .toggle{
+    transform:rotate(90deg);
+    font-weight:100;
+  }
+  li.is-opened .el-submenu__title{
+  >span{
+    color:#409EFF;
+  }
+  >i{
+    color:#409EFF;
+  }
+  transition:all 500ms ease-in-out;
+  }
+  /deep/li .el-submenu__title:hover{
+  >span{
+    color:#fff;
+  }
+  >i{
+    color:#fff;
+  }
+  background-color:#409EFF !important;
+  }
+  ul.el-menu--inline>li{
+    text-align:right;
+  }
+  li.is-active{
+    color:#409EFF;
+  >span{
+    color:#409EFF;
+  }
+  >i{
+    color:#409EFF;
+  }
+  }
+  .el-menu-item{
+    transition:all 500ms ease-in-out;
+  }
+  .el-menu-item:hover{
+  >span{
+    color:#fff;
+  }
+  >i{
+    color:#fff;
+  }
+  background-color:#409EFF !important;
+  }
 </style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,20 +1,45 @@
 <template>
-    <div>
-        <h1>Welcome</h1>
+    <div class="welcome-container">
+        <h1 class='welcome-font-title'>Welcome</h1>
+        <p class="welcome-font-name">电商管理系统</p>
     </div>
 </template>
 
 <script>
-export default {
+  export default {
     name:'Welcome',
     data(){
-        return{
+      return{
 
-        }
+      }
     }
-}
+  }
 </script>
 
 <style lang="less" scoped>
-
+    .welcome-container{
+        width:100%;
+        height:100%;
+        display:flex;
+        flex-wrap:wrap;
+        justify-content:center;
+        align-items:center;
+        align-content: center;
+        user-select:none;
+    }
+    .welcome-font-title{
+        width:100%;
+        font-size:4vmax;
+        margin:0;
+        padding:0;
+        color:#409EFF;
+    }
+    .welcome-font-name{
+        width:100%;
+        font-size:3vmax;
+        font-weight:100;
+        margin:0;
+        padding:0;
+        color:#409EFF;
+    }
 </style>
